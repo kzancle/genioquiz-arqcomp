52b048f29ce34e23957e83cad4c371f26f249856
kzancle <kevinsucle@gmail.com>
2026-06-03 12:33:05 -0300

    ADD project to github

---
+++ dados.js
@@ -0,0 +1,130 @@
+let optionsArray = [
+    {
+        "question": "Dentro do seu PC, existe uma peça que é responsável por processar e execeutar as instruções, e no dialeto popular chamamos o gabinete do desktop com o nome dela. Qual o seu nome?",
+        "a": "CPU",
+        "b": "RAM",
+        "c": "Cérebro",
+        "d": "Jarvis",
+        "answer": "CPU"
+    },
+    {
+        "question": "A CPU é composta por diversas partes, dentre elas, uma executa as operações lógicas e aritméticas. Qual sua alcunha?",
+        "a": "ULM",
+        "b": "Contador",
+        "c": "Pitágoras",
+        "d": "ULA",
+        "answer": "ULA"
+    },
+    {
+        "question": "Ela é o flash das memórias, a mais rápida de todas, embora sua capacidade de armazenamento de dados seja pequena, ela é poderosa. Qual o nome desse tipo de memória?",
+        "a": "Rapid",
+        "b": "Registradores",
+        "c": "Thunder",
+        "d": "ZOOM",
+        "answer": "Registradores"
+    },
+    {
+        "question": "Há uma memória que é responsável por armazenar informações enquanto o computador está ligado, porém ao desligá-lo ela é apagada, similar ao apagador de memória de MIB! Qual o seu nome?",
+        "a": "1155 do ET",
+        "b": "Volátil",
+        "c": "RAM",
+        "d": "ROM",
+        "answer": "RAM"
+    },
+    {
+        "question": "Existe um tipo de memória que é o “checklist” da BIOS, que guarda dados essenciais para o funcionamento do sistema, de modo que é permitida apenas a leitura. Que memória é essa?",
+        "a": "RAM",
+        "b": "RIM",
+        "c": "REM",
+        "d": "ROM",
+        "answer": "ROM"
+    },
+    {
+        "question": "No universo das memórias, umas delas consegue manter informações salvas nela, mesmo sem energia, além de que pode ser atualizada. Que memória é essa?",
+        "a": "Speed",
+        "b": "Flash",
+        "c": "Fast",
+        "d": "Zip",
+        "answer": "Flash"
+    },
+    {
+        "question": "Uma memória poderosa, caapz de armazenar dados por cerca de 20 anos e pode ser lida infinitas vezes, qual o nome dela?",
+        "a": "Eprom",
+        "b": "EDOM",
+        "c": "HD",
+        "d": "Cérebro",
+        "answer": "Eprom"
+    },
+    {
+        "question": "Qual o nome técnico para as memórias convencionais, como HD e SSD? ",
+        "a": "MEMZ",
+        "b": "Memória de peixe",
+        "c": "Memória de Massa",
+        "d": "Memória Permanente",
+        "answer": "Memória de Massa"
+    },
+    {
+        "question": "Existe um atalho que dispositivos IO podem utilizar para comunicarem-se diretamente com a memória RAM, sem passar pela CPU. Qual é o nome desse atalho?",
+        "a": "DFL",
+        "b": "SLA",
+        "c": "DMA",
+        "d": "TIL",
+        "answer": "DMA"
+    },
+    {
+        "question": "É um barramento que serve para transportar endereços da CPU para a memória RAM e dispositivos IO. Qual o nome dele?? ",
+        "a": "Barramento de Local",
+        "b": "Barramento de Endereço",
+        "c": "Barramento de Locate",
+        "d": "Barramento de CEP",
+        "answer": "Barramento de Endereço"
+    },
+    {
+        "question": "É responsável por transferir “xxxxs ” de uma ponta a outra, um verdadeiro ônibus da arquitetura!",
+        "a": "Barramento de Crudis",
+        "b": "Barramento de Clouds",
+        "c": "Barramento de Dados",
+        "d": "Barramento de HDDs",
+        "answer": "Barramento de Dados"
+    },
+    {
+        "question": "Linha de processadores intel lançada em 2009, a escolha perfeita para usuário intermediários! Que linha é essa?",
+        "a": "I5",
+        "b": "I9",
+        "c": "Intel CORE 2",
+        "d": "I3",
+        "answer": "I5"
+    },
+    {
+        "question": "Linha de processadores que possui um bom desempenho e especificações melhores que os i5. Que linha é essa?",
+        "a": "Ryzen 5 3500G",
+        "b": "I3",
+        "c": "I5",
+        "d": "I7",
+        "answer": "I7"
+    },
+    {
+        "question": "Um processador que possui dois núcleos físicos é chamado de?",
+        "a": "Dualcore",
+        "b": "Dualhearth",
+        "c": "Dualtask",
+        "d": "Dualway",
+        "answer": "Dualcore"
+    },
+    {
+        "question": "Um processador que possui quatro núcleos físicos é conhecido como:",
+        "a": "Quadmind",
+        "b": "Quadway",
+        "c": "Quadcore",
+        "d": "Quadajato",
+        "answer": "Quadcore"
+    },
+    {
+        "question": "Complete a sentença: ______ é o nome de uma linha de controle em eletrônica digital usada para selecionar um (ou um conjunto de) circuitos integrados (comumente chamados de 'chips') dentre vários conectados ao mesmo barramento do computador.",
+        "a": "CTU",
+        "b": "CLM",
+        "c": "Chip Choose",
+        "d": "Chip Select",
+        "answer": "Chip Select"
+    },
+]
